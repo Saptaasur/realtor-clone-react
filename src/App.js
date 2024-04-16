415a32d8867d771716dcbe5094c3ef020ae81b2a
unknown <saptarshiiiiiiiiii@gmail.com>
2024-04-16 09:49:14 +0530

    create the header component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
-import {BrowserRouter as Router,Routes, Route} from "react-router-dom"
+import {BrowserRouter as Router,Routes, Route} from "react-router-dom";
 import Home from "./pages/Home";
 import Profile from "./pages/Profile";
 import SignUp from "./pages/SignUp";
 import SignIn from "./pages/SignIn";
 import ForgotPassword from "./pages/ForgotPassword";
 import Offers from "./pages/Offers";
+import Header from "./components/Header";
 function App() {
   return (
     <>
     <Router>
+      <Header/>
       <Routes>
         <Route path="/" element ={<Home/>} />
-        <Route path="/Profile" element ={<Profile/>} />
-        <Route path="/SignUp" element ={<SignUp/>} />
-        <Route path="/SignIn" element ={<SignIn/>} />
-        <Route path="/ForgotPassword" element ={<ForgotPassword/>} />
-        <Route path="/Offers" element ={<Offers/>} />
+        <Route path="/profile" element ={<Profile/>} />
+        <Route path="/sign-Up" element ={<SignUp/>} />
+        <Route path="/sign-In" element ={<SignIn/>} />
+        <Route path="/forgot-Password" element ={<ForgotPassword/>} />
+        <Route path="/offers" element ={<Offers/>} />
       </Routes>
     </Router>
     </>
