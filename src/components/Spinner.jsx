abc44f6467b64b4de3cecc783a1ab5027c6c308e
unknown <saptarshiiiiiiiiii@gmail.com>
2024-04-20 09:16:27 +0530

    add functionality including inchange and onsubmit function to the createlisting page

--- src/components/Spinner.jsx
+++ src/components/Spinner.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import spinner from "../assets/svg/spinner.svg"
+import spinner from "../assets/svg/spinner.svg";
 
 export default function Spinner() {
   return (
     <div className="bg-black bg-opacity-50 flex items justify-center fixed left-0 right-0 bottom-0 top-0 z-40">
         <div>
-            <img src={spinner} alt="Loading..." className='h-24' />
+            <img src={spinner} alt="Loading..." className='h-24 items-center' />
         </div>
     </div>
   )
